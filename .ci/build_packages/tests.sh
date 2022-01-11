de2f3d0b59bd9c4d3ba2d87321acf9451a7429d7
Thales Macedo Garitezi <thalesmg@gmail.com>
2022-01-11 11:47:45 -0300

    fix(ci): raspbian arch is arm, not arm64

--- .ci/build_packages/tests.sh
+++ .ci/build_packages/tests.sh
@@ -20,6 +20,7 @@ esac
 
 PACKAGE_NAME="${1}"
 PACKAGE_TYPE="${2}"
+ARCH="${3}"
 
 export DEBUG=1
 export CODE_PATH=${CODE_PATH:-"/emqx"}
@@ -47,17 +48,20 @@ if ! [ -f "$PACKAGE_FILE" ]; then
     echo "$PACKAGE_FILE is not a file"
 fi
 
-case "$(uname -m)" in
+if [ -z "$ARCH" ]
+then
+  case "$(uname -m)" in
     x86_64)
-        ARCH='amd64'
-        ;;
+      ARCH='amd64'
+      ;;
     aarch64)
-        ARCH='arm64'
-        ;;
+      ARCH='arm64'
+      ;;
     arm*)
-        ARCH=arm
-        ;;
-esac
+      ARCH=arm
+      ;;
+  esac
+fi
 export ARCH
 
 emqx_prepare(){
